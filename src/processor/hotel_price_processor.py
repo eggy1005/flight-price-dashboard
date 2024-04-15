0dfb91e8bddb33cdd13f7db609a317981772c693
Fred <fred_wong_88@hotmail.com>
2024-04-16 00:15:50 +0100

    join hotel and flight price

--- src/processor/hotel_price_processor.py
+++ src/processor/hotel_price_processor.py
@@ -8,9 +8,9 @@ def consume_data():
     # Create streaming environment
     env = StreamExecutionEnvironment.get_execution_environment()
 
-    settings = EnvironmentSettings.new_instance()\
-                      .in_streaming_mode()\
-                      .build()
+    settings = EnvironmentSettings.new_instance() \
+        .in_streaming_mode() \
+        .build()
 
     # create table environment
     tbl_env = StreamTableEnvironment.create(stream_execution_environment=env,
@@ -20,14 +20,40 @@ def consume_data():
     kafka_jar = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                              'flink-sql-connector-kafka-3.1.0-1.18.jar')
     kafka_jar_2 = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                             'kafka-clients-3.7.0.jar')
-    tbl_env.get_config()\
-            .get_configuration()\
-            .set_string("pipeline.jars", "file://{};file://{}".format(kafka_jar, kafka_jar_2))
-
+                               'kafka-clients-3.7.0.jar')
+    tbl_env.get_config() \
+        .get_configuration() \
+        .set_string("pipeline.jars", "file://{};file://{}".format(kafka_jar, kafka_jar_2))
+# SPLIT_INDEX(name_rating_city_country, '_', 0) AS name,
+# SPLIT_INDEX(name_rating_city_country, '_', 1) AS rating,
+# SPLIT_INDEX(name_rating_city_country, '_', 2) AS city,
+# SPLIT_INDEX(name_rating_city_country, '_', 3) AS country,
     #######################################################################
     # Create Kafka Source Table with DDL
     #######################################################################
+    src_ddl = """
+        CREATE TABLE HOTELPRICE (
+            name_rating_city_country String,
+            price Double,
+            breakfast_included BOOLEAN,
+            update_timestamp BIGINT
+        ) WITH (
+            'connector' = 'kafka',
+            'topic' = 'hotel-price',
+            'properties.bootstrap.servers' = 'pkc-41wq6.eu-west-2.aws.confluent.cloud:9092',
+            'properties.group.id' = 'flight-usd',
+            'format' = 'json',
+            'scan.startup.mode' = 'earliest-offset',    
+            'properties.client.dns.lookup' ='use_all_dns_ips',
+            'properties.auto.offset.reset' = 'latest',
+            'properties.security.protocol' = 'SASL_SSL',
+            'properties.sasl.mechanism' = 'PLAIN',
+            'properties.sasl.jaas.config' = 'org.apache.kafka.common.security.plain.PlainLoginModule required username="7AICBYXFYBHMF5PJ" password="yylYReFtxTnInxAeng6ubVTs+sMXGBi8Hz0AdAIO4eVB8/LXJHxKASyh51B0eWu8";'
+        )
+    """
+
+    tbl_env.execute_sql(src_ddl)
+
     src_ddl = """
         CREATE TABLE FLIGHTPRICE (
             airline STRING,
@@ -51,17 +77,26 @@ def consume_data():
             'properties.sasl.jaas.config' = 'org.apache.kafka.common.security.plain.PlainLoginModule required username="7AICBYXFYBHMF5PJ" password="yylYReFtxTnInxAeng6ubVTs+sMXGBi8Hz0AdAIO4eVB8/LXJHxKASyh51B0eWu8";'
         )
     """
-
     tbl_env.execute_sql(src_ddl)
 
     # create and initiate loading of source Table
+    tbl = tbl_env.from_path('HOTELPRICE')
     tbl = tbl_env.from_path('FLIGHTPRICE')
 
     average_sql_processor = """
         SELECT
-        airline, departure, arrival, flightno, price, currency
+        F.airline,
+        SPLIT_INDEX(name_rating_city_country, '_', 0) as name, 
+        SPLIT_INDEX(name_rating_city_country, '_', 2) as city, 
+        SPLIT_INDEX(name_rating_city_country, '_', 3) as country, 
+        F.price,
+        H.price,
+        F.price + H.price as total, 
+        breakfast_included,
+        TO_TIMESTAMP_LTZ(F.update_timestamp, 3) AS readable_time
         FROM
-        FLIGHTPRICE
+        HOTELPRICE H JOIN FLIGHTPRICE F ON 
+        SPLIT_INDEX(name_rating_city_country, '_', 2) = F.arrival
         """
     average_price_tbl = tbl_env.sql_query(average_sql_processor)
 
